743b927eead5f703c0b4bff8b50c422222601af2
binivazqua <binizavazquez@gmail.com>
2024-06-17 15:02:50 -0600

    ensure initialized done

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+/* ++++++++++++++++++++++++ FIREBASE ++++++++++++++++++++++++++ */
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final Future<FirebaseApp> _firebase = Firebase.initializeApp();
+
+  MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        home: FutureBuilder(
+            future: _firebase,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print('There is an error somewhere around here...');
+                return Text('Somethin went WRONG! PIPIPI');
+              } else if (snapshot.hasData) {
+                return Scaffold(
+                  backgroundColor: Colors.black,
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
   }
 }
