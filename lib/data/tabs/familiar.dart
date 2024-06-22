e27b4b91d25f06612b0df8e7138fe1be150f7b72
binivazqua <binizavazquez@gmail.com>
2024-06-21 23:42:54 -0600

    started familiar perception dropdown

--- lib/data/tabs/familiar.dart
+++ lib/data/tabs/familiar.dart
@@ -1,3 +1,4 @@
+import 'package:bejoy/components/userData/dropDown.dart';
 import 'package:flutter/material.dart';
 
 class percepcionFamiliar extends StatefulWidget {
@@ -8,13 +9,38 @@ class percepcionFamiliar extends StatefulWidget {
 }
 
 class _percepcionFamiliarState extends State<percepcionFamiliar> {
+  String _default1 = '';
+  final List<String> escala_de_identidad = [
+    'Totalmente en desacuerdo',
+    'En desacuerdo',
+    'No se',
+    'De acuerdo',
+    'Totalmente de acuerdo',
+    ''
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.orange[100],
       body: Center(
-        child: Text('Percepción Familiar'),
-      ),
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(
+              'Responde respecto al nivel en el que te sientas identificada',
+              textAlign: TextAlign.center,
+            ),
+            dropdownMenu(
+              helper: '',
+              defaultValue: _default1,
+              list: escala_de_identidad,
+              theme: 'Escala',
+              width: 300,
+              textColor: Colors.purple,
+              onChanged: (p0) => {_default1 = p0!},
+            ),
+          ],
+        ),
+      )),
     );
   }
 }
