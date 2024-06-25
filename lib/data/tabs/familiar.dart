f1029035561a6e0bb989310e3c6c985b16921e06
binivazqua <binizavazquez@gmail.com>
2024-06-25 12:46:03 -0600

    percepcion familiar quest part to firebase done

--- lib/data/tabs/familiar.dart
+++ lib/data/tabs/familiar.dart
@@ -1,6 +1,8 @@
 import 'package:bejoy/components/userData/dropDown.dart';
 import 'package:bejoy/components/userData/longTextField.dart';
 import 'package:bejoy/components/userData/shortTextField.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class percepcionFamiliar extends StatefulWidget {
@@ -11,11 +13,70 @@ class percepcionFamiliar extends StatefulWidget {
 }
 
 class _percepcionFamiliarState extends State<percepcionFamiliar> {
-  final TextEditingController _confianza = new TextEditingController();
-  final TextEditingController _yosoyparamifamilia = new TextEditingController();
+  TextEditingController _confianza = new TextEditingController();
+  TextEditingController _yosoyparamifamilia = new TextEditingController();
 
-  String _default1 = '';
-  String _default2 = '';
+  @override
+  void dispose() {
+    _confianza.dispose();
+    _yosoyparamifamilia.dispose();
+    super.dispose();
+  }
+
+  FirebaseFirestore database = FirebaseFirestore.instance;
+
+  Future<void> _saveResponses(User? currentUser) async {
+    if (currentUser == null) {
+      print('No user was found. Log in.');
+      return;
+    }
+
+    // Debugging purposes:
+    print('Apoyo familiar: ${_apoyofamiliar}');
+    print('Me he sentido juzgada: ${_juiciofamiliar}');
+    print('Me he sentido incomprendida: ${_incomprensionfamiliar}');
+    print('Me siento cómoda hablando sobre mi TCA: ${_comodidadhablando}');
+    print('Cuando no puedo más, acudo con: ${_apoyoprincipal}');
+    print('Razón de confianza: ${_confianza.text.trim()}');
+    print('Yo soy para mi familia: ${_yosoyparamifamilia.text.trim()}');
+
+    try {
+      DocumentReference userDocRef =
+          database.collection('users').doc(currentUser?.uid);
+
+      // Add data to the 'general perception' subcollection
+      await userDocRef
+          .collection('initial_diagnostic')
+          .doc('familiar_perception')
+          .set({
+        'Apoyo familiar': _apoyofamiliar,
+        'Me he sentido juzgada': _juiciofamiliar,
+        'Me he sentido incomprendida': _incomprensionfamiliar,
+        'Me siento cómoda hablando sobre mi TCA': _comodidadhablando,
+        'Cuando no puedo más, acudo con': _apoyoprincipal,
+        'Razón de confianza': _confianza.text.trim(),
+        'Yo soy para mi familia': _yosoyparamifamilia.text.trim(),
+      });
+      print('Info sent to database.');
+    } catch (error) {
+      print('There was an error sending data: $error');
+    }
+  }
+
+  Future<void> sendData() async {
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      await _saveResponses(user);
+    } catch (error) {
+      print('Encountered an error sending data: $error.');
+    }
+  }
+
+  String _apoyofamiliar = '';
+  String _juiciofamiliar = '';
+  String _incomprensionfamiliar = '';
+  String _comodidadhablando = '';
+  String _apoyoprincipal = '';
 
   final List<String> escala_de_identidad = [
     'Totalmente en desacuerdo',
@@ -57,12 +118,12 @@ class _percepcionFamiliarState extends State<percepcionFamiliar> {
               ),
               dropdownMenu(
                 helper: '',
-                defaultValue: _default1,
+                defaultValue: _apoyofamiliar,
                 list: escala_de_identidad,
                 theme: 'Escala',
                 width: 310,
                 textColor: Colors.purple,
-                onChanged: (p0) => {_default1 = p0!},
+                onChanged: (p0) => {_apoyofamiliar = p0!},
               ),
               Text(
                   'Durante mi recuperación, me he sentido juzgada por algún miembro de mi familia.'),
@@ -71,12 +132,12 @@ class _percepcionFamiliarState extends State<percepcionFamiliar> {
               ),
               dropdownMenu(
                 helper: '',
-                defaultValue: _default1,
+                defaultValue: _juiciofamiliar,
                 list: escala_de_identidad,
                 theme: 'Escala',
                 width: 310,
                 textColor: Colors.purple,
-                onChanged: (p0) => {_default1 = p0!},
+                onChanged: (p0) => {_juiciofamiliar = p0!},
               ),
               Text('Me siento incomprendida por algún miembro de mi familia.'),
               SizedBox(
@@ -84,12 +145,12 @@ class _percepcionFamiliarState extends State<percepcionFamiliar> {
               ),
               dropdownMenu(
                 helper: '',
-                defaultValue: _default1,
+                defaultValue: _incomprensionfamiliar,
                 list: escala_de_identidad,
                 theme: 'Escala',
                 width: 310,
                 textColor: Colors.purple,
-                onChanged: (p0) => {_default1 = p0!},
+                onChanged: (p0) => {_incomprensionfamiliar = p0!},
               ),
               Text('Me siento cómoda hablando sobre mi TCA con mi familia.'),
               SizedBox(
@@ -97,12 +158,12 @@ class _percepcionFamiliarState extends State<percepcionFamiliar> {
               ),
               dropdownMenu(
                 helper: '',
-                defaultValue: _default1,
+                defaultValue: _comodidadhablando,
                 list: escala_de_identidad,
                 theme: 'Escala',
                 width: 310,
                 textColor: Colors.purple,
-                onChanged: (p0) => {_default1 = p0!},
+                onChanged: (p0) => {_comodidadhablando = p0!},
               ),
               Text(
                   'Durante mi proceso, cuando siento que no puedo más, acudo con: '),
@@ -111,12 +172,12 @@ class _percepcionFamiliarState extends State<percepcionFamiliar> {
               ),
               dropdownMenu(
                 helper: '',
-                defaultValue: _default2,
+                defaultValue: _apoyoprincipal,
                 list: fuentes_de_apoyo,
                 theme: 'Apoyo',
                 width: 310,
                 textColor: Colors.purple,
-                onChanged: (p0) => {_default2 = p0!},
+                onChanged: (p0) => {_apoyoprincipal = p0!},
               ),
               Text(
                   '¿Cuál es la razón principal por la que te sientes en confianza con esa persona?'),
@@ -139,7 +200,11 @@ class _percepcionFamiliarState extends State<percepcionFamiliar> {
                 label: '',
                 filled: true,
                 fillColor: Colors.white,
-              )
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ElevatedButton(onPressed: sendData, child: Text('Send')),
             ],
           ),
         ),
