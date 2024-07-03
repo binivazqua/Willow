df11b486491520202e2c3884178a3ef0cb9e72af
binivazqua <binizavazquez@gmail.com>
2024-07-02 22:35:58 -0600

    color palette added

---
+++ lib/design/colors/palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final Color deepTurquoise = Color.fromARGB(255, 83, 131, 146);
+final Color turquoise = Color.fromARGB(255, 98, 149, 162);
+final Color lightTurquoise = Color.fromARGB(255, 128, 185, 173);
+final Color green = Color.fromARGB(255, 179, 226, 167);
