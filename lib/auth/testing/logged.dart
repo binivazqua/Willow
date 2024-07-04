415fdd461b8f034f05ca9728c39018ff94e12c4d
binivazqua <binizavazquez@gmail.com>
2024-07-03 19:18:45 -0600

    styling logged page

--- lib/auth/testing/logged.dart
+++ lib/auth/testing/logged.dart
@@ -1,4 +1,7 @@
+import 'package:bejoy/components/userData/turquoiseButton.dart';
 import 'package:bejoy/data/tabs/tabs.dart';
+import 'package:bejoy/design/colors/palette.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -11,31 +14,64 @@ class loggedPage extends StatefulWidget {
 
 class _loggedPageState extends State<loggedPage> {
   final user = FirebaseAuth.instance.currentUser;
+  FirebaseFirestore database = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: IntrinsicWidth(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text('signed in as ${user?.email}'),
-              MaterialButton(
-                  color: Colors.purple,
-                  child: Text('Sign Out'),
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  }),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => userDiagnosticTest()));
-                },
-                color: Colors.purple,
-                child: Text('Go to data'),
+              Image(
+                image: AssetImage('lib/design/logos/willow_tree.png'),
+                width: 100,
+                height: 100,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text('hi, ${user?.email}!'),
+              SizedBox(
+                height: 20,
+              ),
+              Text('We are just about to kickstart your journey!'),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Please fill out the following diagnostic test so that our team can discover how to help you throught your process',
+                softWrap: true,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              IntrinsicWidth(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Turquoisebutton(
+                      txt: 'Sign out',
+                      op: () {
+                        FirebaseAuth.instance.signOut();
+                      },
+                    ),
+                    Turquoisebutton(
+                      txt: 'Go to test',
+                      op: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => userDiagnosticTest()));
+                      },
+                    ),
+                  ],
+                ),
               )
             ],
           ),
