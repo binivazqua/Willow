bc1c3c33b4317af4b98fcafd47583d1773174c9b
binivazqua <binizavazquez@gmail.com>
2024-12-20 19:52:40 -0600

    created MyUser class

---
+++ lib/pages/foodlog/components/models/user.dart
@@ -0,0 +1,9 @@
+class MyUser {
+  String name;
+  String gender;
+
+  MyUser({String n, String g}) {
+    name = n;
+    gender = g;
+  }
+}
---
+++ lib/pages/userPage.dart
@@ -0,0 +1,17 @@
+import 'package:bejoy/pages/foodlog/components/models/user.dart';
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({super.key});
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  MyUser user = MyUser(n: "Panchita", g: "female");
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
